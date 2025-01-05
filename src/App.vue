f1a89268c9c4eb9e4eab033fd398ba518c6ab1fe
Gustavo Escobar Cobos <gustavo.escobar@outlook.com>
2025-01-05 19:49:57 -0400

    Colocando imagen de fondo

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,7 @@ import { RouterView } from 'vue-router'
 </script>
 
 <template>
-  <div class="bg-gray-100 min-h-screen">
+  <div class="bg-gray-100 min-h-screen" id="app-container">
 
     <header class="text-black p-4">
       <h1 class="text-2xl font-bold">Aquí iría el nav</h1>
@@ -17,5 +17,9 @@ import { RouterView } from 'vue-router'
 </template>
 
 <style scoped>
+
+  #app-container {
+    @apply bg-[url('./assets/img/movie-backgrounds/breaking-bad.jpg')] md:bg-[url('./assets/img/movie-backgrounds/breaking-bad-md.jpg')] bg-cover bg-no-repeat bg-center;
+  }
 
 </style>
