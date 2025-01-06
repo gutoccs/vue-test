dc6e41c1702b2892d00ed3a7cf7ecb69bab07d28
Gustavo Escobar Cobos <gustavo.escobar@outlook.com>
2025-01-06 03:33:56 -0400

    Agregando versión simple de sidebar junto a los nuevos botones bars3

--- src/components/NavigationBar.vue
+++ src/components/NavigationBar.vue
@@ -5,10 +5,11 @@
         <div class="absolute inset-y-0 left-0 flex items-center sm:hidden">
           <!-- Mobile menu button-->
           <DisclosureButton class="relative inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white">
-            <span class="absolute -inset-0.5" />
-            <span class="sr-only">Open main menu</span>
-            <Bars3Icon v-if="!open" class="block size-6" aria-hidden="true" />
-            <XMarkIcon v-else class="block size-6" aria-hidden="true" />
+            <span class="absolute -inset-1.5" />
+            <span class="sr-only">View Sidebar</span>
+            <Bars3BottomLeftIcon v-if="isMobile && !open" class="size-7" aria-hidden="true" />
+            <Bars3BottomRightIcon v-if="!isMobile && !open" class="size-7" aria-hidden="true" />
+            <XMarkIcon v-if="open" class="block size-6" aria-hidden="true" />
           </DisclosureButton>
         </div>
         <div class="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start">
@@ -16,11 +17,6 @@
             <span class="text-3xl sm:text-4xl text-lilac-light">
               <span class="font-normal sm:font-semibold">TEST</span>FLIX
             </span>
-          </div>
-          <div class="hidden sm:ml-6 sm:block">
-            <div class="flex space-x-4">
-              <a v-for="item in navigation" :key="item.name" :href="item.href" :class="[item.current ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white', 'rounded-md px-3 py-2 text-sm font-medium']" :aria-current="item.current ? 'page' : undefined">{{ item.name }}</a>
-            </div>
           </div>
         </div>
         <div class="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
@@ -34,12 +30,13 @@
             </div>
           </div>
 
-          <button type="button" class="relative rounded-full bg-gray-800 p-1 text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800">
+          <DisclosureButton type="button" class="relative rounded-full bg-gray-800 p-1 text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800 hidden sm:block">
             <span class="absolute -inset-1.5" />
             <span class="sr-only">View Sidebar</span>
-            <Bars3BottomLeftIcon v-if="isMobile" class="size-7" aria-hidden="true" />
-            <Bars3BottomRightIcon v-else class="size-7" aria-hidden="true" />
-          </button>
+            <Bars3BottomLeftIcon v-if="isMobile && !open" class="size-7" aria-hidden="true" />
+            <Bars3BottomRightIcon v-if="!isMobile && !open" class="size-7" aria-hidden="true" />
+            <XMarkIcon v-if="open" class="block size-6" aria-hidden="true" />
+          </DisclosureButton>
 
           <button type="button" class="relative rounded-full bg-gray-800 p-1 text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800 hidden sm:block">
             <span class="absolute -inset-1.5" />
@@ -86,7 +83,7 @@
 <script setup>
   import { computed , ref, onMounted, onUnmounted } from 'vue';
   import { Disclosure, DisclosureButton, DisclosurePanel, Menu, MenuButton, MenuItem, MenuItems } from '@headlessui/vue'
-  import { ArrowRightCircleIcon, Bars3Icon, BellIcon, XMarkIcon, Bars3BottomRightIcon, Bars3BottomLeftIcon} from '@heroicons/vue/24/outline'
+  import { ArrowRightCircleIcon, BellIcon, XMarkIcon, Bars3BottomRightIcon, Bars3BottomLeftIcon} from '@heroicons/vue/24/outline'
 
   const navigation = [
     { name: 'Dashboard', href: '#', current: true },
