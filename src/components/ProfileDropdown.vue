ebbc13b5fe28b6b12dad1c8bc985356c0a85eb08
Gustavo Escobar Cobos <gustavo.escobar@outlook.com>
2025-01-07 15:25:53 -0400

    Mejorando estilo en dropdown de menu perfil

--- src/components/ProfileDropdown.vue
+++ src/components/ProfileDropdown.vue
@@ -9,15 +9,15 @@
       </MenuButton>
     </div>
     <transition enter-active-class="transition ease-out duration-100" enter-from-class="transform opacity-0 scale-95" enter-to-class="transform opacity-100 scale-100" leave-active-class="transition ease-in duration-75" leave-from-class="transform opacity-100 scale-100" leave-to-class="transform opacity-0 scale-95">
-      <MenuItems class="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black/5 focus:outline-none">
+      <MenuItems class="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-gray-700 py-1 shadow-lg ring-1 ring-black/5 focus:outline-none">
         <MenuItem v-slot="{ active }">
-          <a href="#" :class="[active ? 'bg-gray-100 outline-none' : '', 'block px-4 py-2 text-sm text-gray-700']">Ver Perfil</a>
+          <a href="#" :class="[active ? 'bg-gray-100 outline-none' : '', 'block px-4 py-2 text-sm hover:text-gray-700 text-white']">Ver Perfil</a>
         </MenuItem>
         <MenuItem v-slot="{ active }">
-          <a href="#" :class="[active ? 'bg-gray-100 outline-none' : '', 'block px-4 py-2 text-sm text-gray-700']">Configuración</a>
+          <a href="#" :class="[active ? 'bg-gray-100 outline-none' : '', 'block px-4 py-2 text-sm hover:text-gray-700 text-white']">Configuración</a>
         </MenuItem>
         <MenuItem v-slot="{ active }">
-          <a href="#" :class="[active ? 'bg-gray-100 outline-none' : '', 'block px-4 py-2 text-sm text-gray-700']">Cerrar sesión</a>
+          <a href="#" :class="[active ? 'bg-gray-100 outline-none' : '', 'block px-4 py-2 text-sm hover:text-gray-700 text-white']">Cerrar sesión</a>
         </MenuItem>
       </MenuItems>
     </transition>
